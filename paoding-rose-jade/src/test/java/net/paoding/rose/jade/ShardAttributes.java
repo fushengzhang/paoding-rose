6c21d9fd638b7870028d8b67f38246e9cb9bc621
fusheng.zhang <15210147315@163.com>
2018-12-04 15:58:28 +0800

    修改pom 删除测试

--- paoding-rose-jade/src/test/java/net/paoding/rose/jade/ShardAttributes.java
+++
@@ -1,8 +0,0 @@
-package net.paoding.rose.jade;
-
-public interface ShardAttributes {
-
-    String originTable = "_ShardAttributes.originTable";
-
-    String targetTable = "_ShardAttributes.targetTable";
-}
